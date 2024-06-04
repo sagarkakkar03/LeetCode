44a120bb62c443d2a325f79177479a27af47cf25
sagarkakkar03 <122864251+sagarkakkar03@users.noreply.github.com>
2024-06-04 09:30:44 +0530

    Time: 34 ms (76.19%), Space: 16.7 MB (34.5%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,10 +1,6 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        alphabet_dictionary = {}
-        for alphabet_in_s in s:
-            if alphabet_in_s not in alphabet_dictionary:
-                alphabet_dictionary[alphabet_in_s] = 0
-            alphabet_dictionary[alphabet_in_s] += 1
+        alphabet_dictionary = Counter(s)
         odd = 0
         ans = 0
         for alphabet_in_alphabet_dictionary in alphabet_dictionary:
